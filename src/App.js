cb9538a279af995c6cfe4a3e79881eda79233bd0
Akins20 <ogunbiye@gmail.com>
2023-07-03 11:19:17 +0100

    Created Responsive Site

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='welcome'>
+          <img src={logo} className='logo' alt="logo"/>
+          <h1>Responsive Site</h1>
+          <h4>This is a test responsive site, look around and enjoy.</h4>
+        </div>
+        <form>
+          <h3>Sign in</h3>
+          <label htmlFor="fullName">Full name</label><br/>
+          <input typeof='text' name='fullName' placeholder="Enter your full name..."/><br/>
+          <label htmlFor='email'>Email</label><br/>
+          <input typeof='email' name='email' placeholder='Enter your email...'/><br/>
+          <label htmlFor="password">Password</label><br/>
+          <input typeof='password' name='password' placeholder='8888888'/><br/>
+          <button>Submit</button>
+        </form>
       </header>
     </div>
   );
